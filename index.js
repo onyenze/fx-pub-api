d366d264cd88480e6ff691ded1743de6ce65c272
onyenze <chibuezeonyenze123@gmail.com>
2025-05-07 14:34:20 +0100

    CORS update for frontend URL

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://fx-pipeline-pub.onrender.com',
   methods: ['GET', 'POST'],
   credentials: true
 }));
